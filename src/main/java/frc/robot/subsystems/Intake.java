ce3a8de253d6f5e16bc04259ff6db80ca0c71446
adah25 <adahmeyerhoff@gmail.com>
2024-11-14 18:30:11 -0700

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,3 +6,9 @@ public class Intake extends SubsystemBase{
     CCSparkMax intakeMotor = new CCSparkMax("Intake Motor", "IM", 1, MotorType.kBrushless, IdleMode.kBrake, false);
     CCSparkMax outtakeMotor = new CCSparkMax("Outtake Motor", "OM", 2, MotorType.kBrushless, IdleMode.kBrake, true);
 }
+public void motorIn(){
+    intakeMotor.set(0.5);
+}
+public void motorOut(){
+    outtakeMotor.set(-0.5);
+}
